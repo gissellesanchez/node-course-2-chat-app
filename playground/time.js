0205df98ef4a70e9778a82dc9c3fba6f20e65452
Gisselle.Sanchez <>
2018-12-03 15:41:17 -0600

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+//Jan 1st 1970 00:00:00 am
+var moment=require('moment');
+
+// var date= new Date();
+// console.log(date.getMonth());
+
+var someTimestamp=moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date=moment(createdAt);
+console.log(date.format('h:mm a'));
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,11 @@
+var moment=require('moment');
+
+
 var generateMessage = (from,text )=>{
   return {
     from,
     text,
-    createdAt:new Date().getTime()
+    createdAt:moment().valueOf()
   }
 };
 
@@ -10,8 +13,11 @@ var generateLocatioMessage = (from, latitude,longitude)=>{
     return{
       from,
       url:`https://maps.googleapis.com/maps/api/geocode/json?address=${latitude},${longitude}&key=AIzaSyCnL5NGwK7rGmuhBvqKUmDSK4h6cuknAy0`,
-      createdAt:new Date().getTime()
+      createdAt:moment().valueOf()
     }
 };
+
+
+
 
 module.exports = {generateMessage,generateLocatioMessage};
